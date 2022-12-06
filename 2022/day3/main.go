86f5f55461f09081b6b776eabc3b739d2fb7de6d
matah <selma.ardelean@gmail.com>
2022-12-06 22:48:39 +0000

    day3: read input

---
+++ 2022/day3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+)
+
+//go:embed input
+var input []byte
+
+func main() {
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
