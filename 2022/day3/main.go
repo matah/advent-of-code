8bc088386a56d9bab6d8fd4b7f98360220d1cddc
matah <selma.ardelean@gmail.com>
2022-12-06 23:18:55 +0000

    day3: part1

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
+	"strings"
 )
 
 //go:embed input
@@ -12,8 +13,34 @@ var input []byte
 
 func main() {
 	scanner := bufio.NewScanner(bytes.NewReader(input))
+	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		half := len(line) / 2
+		firstCompartment := line[:half]
+		secondCompartment := line[half:]
+		duplicate := findDuplicate(firstCompartment, secondCompartment)
+		total += priority(duplicate)
 	}
+
+	fmt.Println(total)
+}
+
+func findDuplicate(s1 string, s2 string) rune {
+	for _, c := range s1 {
+		if strings.ContainsRune(s2, c) {
+			return c
+		}
+	}
+
+	panic("No duplicate found")
+}
+
+func priority(c rune) int {
+	if c > 'a' {
+		return int(c-'a') + 1
+	}
+
+	return int(c-'A') + 27
 }
